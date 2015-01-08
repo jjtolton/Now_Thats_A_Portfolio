ec47eae4395cf2222308c8ca57a4940b76a5f78a
HiImJayHireMe <jjtolton@gmail.com>
2015-01-08 14:58:32 -0500

    eliminate_redundant working

--- Project_Spoiler/PE_5_01.py
+++ Project_Spoiler/PE_5_01.py
@@ -6,12 +6,15 @@ Created on Wed Jan 07 21:30:47 2015
 """
 
 from numpy import array, argmax, vectorize
+from MyDevTools.MyMathTools import get_factors
 
 def main():
 #    test()
 #    assert numpy.any(find_divisible(xrange(11)))
 #    print find_divisible(xrange(1,11))
-    pass
+#    print set(get_factors(20))
+    print eliminate_redundant(xrange(1,21))
+    
 
 def find_divisible(divisors):
     start, stop = 5, 7  #to prevent infinite loop
@@ -28,8 +31,21 @@ def divisible(divisors):
     is_0 = vectorize(lambda x: x == 0)
     return lambda x: is_0(div_all(x)).any()
     
-    
-    
+def eliminate_redundant(nums):
+    largest_factor = max(nums)
+    factors = sorted(nums)
+    nonredundant = []
+    get_set = lambda x: set(get_factors(x))
+    def subset(x,y): 
+        return get_set(x).issubset(get_set(y))        
+    res = []
+    while factors:
+        test = factors.pop()
+        res += [test]
+        good_2_go = [factor for factor in factors if not subset(factor,test)]
+        factors = good_2_go        
+    return res
+
 
 def test():
     pass

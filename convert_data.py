b5b6b693933cef548c17fd9eca3243372fe74658
HiImJayHireMe <jjtolton@gmail.com>
2015-01-13 03:56:07 -0500

    commit before merge

---
+++ convert_data.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 13 03:49:47 2015
+
+@author: J
+"""
+
+"""example output"""
+opticsTimes = [   #start, end
+                [13,13.83],   #M
+                [0,0],        #T
+                [13,13.83],   #W
+                [0,0],        #R
+                [13,13.83]    #F
+              ]
+
+"""example input"""

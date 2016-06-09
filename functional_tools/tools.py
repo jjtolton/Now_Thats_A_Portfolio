fae6d1eda206d9a23d31f8c2cfe1270e3bd861fb
Jay <jatolton@cisco.com>
2016-06-08 23:17:32 -0400

    moved older code to "old code" folder; fixed assoc_in in tools and added keys2dict and recursive_dict_merge

--- functional_tools/tools.py
+++ functional_tools/tools.py
@@ -1,7 +1,7 @@
 import itertools
 
 
-def update(fn):
+def unpack(fn):
     def explode(args):
         return fn(*args)
 
@@ -21,7 +21,7 @@ def dissoc(d, *ks):
 
 
 def merge_with(fn, *dicts):
-    return reduce(lambda d, x: update(lambda k, v:
+    return reduce(lambda d, x: unpack(lambda k, v:
                                       assoc(d, k, fn(d[k], v)) if k in d else
                                       assoc(d, k, v))(x),
                   itertools.chain(*itertools.imap(lambda _d: _d.items(), dicts)), {})
@@ -32,9 +32,30 @@ def merge_with_default(fn, default=None, *dicts):
                            itertools.chain(*itertools.imap(lambda d: d.items(), dicts))}, *dicts)
 
 
-def assoc_in(d, val, k, *ks):
-    return merge_with(lambda a, b: merge(a, b), d, reduce(lambda x, y: {y: x}, ([k] + list(ks) + [val])[::-1]))
 
+def assoc_in(d, val, k, *ks):
+    d1 = keys2dict(val, k, ks)
+    return recursive_dict_merge(d, d1)
+
+
+def terminal_dict(*ds):
+    return are_dicts(ds) and all(map(lambda x: are_dicts(x.values()), ds))
+
+
+
+def terminal_dicts(*ds):
+    return all(map(terminal_dict, ds))
+
+
+def recursive_dict_merge(*ds):
+    return merge_with(lambda a, b: recursive_dict_merge(a, b) if not terminal_dicts(a, b) else merge(a, b), *ds)
+
+
+
+
+def keys2dict(val, k, ks):
+    v_in = reduce(lambda x, y: {y: x}, ([k] + list(ks) + [val])[::-1])
+    return v_in
 
 def are_instances(items, types):
     return all(map(lambda x: isinstance(x, types), items))
@@ -77,18 +98,23 @@ def main():
     b = {A: 1}
     c = {A: 2}
 
-    print(merge_with_default(lambda x, y: x + [y], [], a, b, c))
-    print(merge_with_default(lambda x, y: x + y, 0, a, b, c))
-    print(itemmap(lambda x, y: (y, x), merge_with_default(lambda x, y: x + y, 0, a, b, c)))
-    print(dissoc(a, A))
-    print(a)
+    # print(merge_with_default(lambda x, y: x + [y], [], a, b, c))
+    # print(merge_with_default(lambda x, y: x + y, 0, a, b, c))
+    # print(itemmap(lambda x, y: (y, x), merge_with_default(lambda x, y: x + y, 0, a, b, c)))
+    # print(dissoc(a, A))
+    # print(a)
 
     # {3: {2: 1}}
     # {3: {A: B}}
     # {3: {2: 1. A: B}}
 
-    print(assoc_in({3: {2: 1}}, 'B', 3, 'A'))
-    print(supassoc_in({4: {3: 5}}, 'A', 4, 3, 3, 1))
+    print(assoc_in({3: {2: 1, 4: 5}}, 'B', 3, 'A', 5, 10))
+    print(assoc_in({4: {5: {6: 7}}}, {3:'X'}, 4, 5))
+
+    # print(merge_with_default(lambda a, b: merge(a, b) if isinstance(b, dict) else assoc(a, b, a[b]),
+    #                          {}, {4: {5: {6: 7}}}, {4: {5: {3: 2}}}))
+    # print(supassoc_in({4: {3: 5}}, 'A', 4, 3, 3, 1))
+    print(assoc_in(a, 5, 4, 3))
 
 if __name__ == '__main__':
     main()

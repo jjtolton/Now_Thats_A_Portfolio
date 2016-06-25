9c2498422647f0972e5b606e1f21f086dbdd55ce
Jay <jatolton@cisco.com>
2016-06-25 16:32:07 -0400

    initial commit; added first project euler problem

---
+++ clojure_work/src/clojure_work/core.clj
@@ -0,0 +1,3 @@
+(ns clojure-work.core
+  (:require [clojure-work.project-euler.pe1 :as pe1]))
+
---
+++ clojure_work/test/clojure_work/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-work.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-work.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

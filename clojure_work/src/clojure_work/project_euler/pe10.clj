44e918d113a6fbae8bf3b4ffb4bd0940f42d18de
Jay <jatolton@cisco.com>
2016-07-16 20:07:46 -0400

    added pe10

---
+++ clojure_work/src/clojure_work/project_euler/pe10.clj
@@ -0,0 +1,28 @@
+; Summation of primes
+; Problem 10
+; The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+;
+; Find the sum of all the primes below two million.
+;
+;
+; Answer:
+; 142913828922
+
+(ns clojure-work.project-euler.pe10)
+
+(defn prime?
+  [n]
+  (cond (< n 2) false
+        (some #{n} [2 3]) true
+        (= 0 (mod n 2)) false
+        :else (let [base (Math/round (Math/sqrt n))
+                    upto (if (= 0 (mod base 2))
+                             (+ 1 base)
+                             base)]
+        (loop [x upto]
+          (cond (<= x 2) true
+                   (= 0 (mod n x)) false
+                   :else (recur (- x 2)))))))
+
+;; print result
+(time (println (reduce + (take-while #(<= % 2000000) (filter prime? (cons 2 (range 1 2000001 2)))))))

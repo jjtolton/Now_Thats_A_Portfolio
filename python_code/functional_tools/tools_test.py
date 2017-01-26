290410dbe8aa1e10c3b360de14198eb8eab4f814
J.J. Tolton <jjtolton@gmail.com>
2017-01-26 13:38:57 -0500

    fixed py2to3 compatability issue

--- python_code/functional_tools/tools_test.py
+++ python_code/functional_tools/tools_test.py
@@ -72,7 +72,8 @@ class FuncyToolsTest(unittest.TestCase):
         self.assertEqual(expected_res, res)
 
     def test_merge_with_default_sums_or_adds(self):
-        a, *_ = self.ds
+        a = self.ds[:1]
+
         expected_res = {1: 4, 3: 8, 4: 10}
         res = merge_with_default(lambda x, y: x + y, 0, *[a, a])
         self.assertEqual(expected_res, res)

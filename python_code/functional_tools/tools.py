c1d9964ef8054cd81089a00b2c94b2b2577385b8
J.J. Tolton <jjtolton@gmail.com>
2017-11-10 20:39:07 -0500

    updated tools to reflect naga v0.3

--- python_code/functional_tools/tools.py
+++ python_code/functional_tools/tools.py
@@ -1,7 +1,14 @@
+import collections
 import itertools
+import types
 from functools import reduce, partial
 
 seq_types = list, tuple, str
+
+
+class nil:
+    def __bool__(self):
+        return False
 
 
 def rreduce(fn, seq, default=None):
@@ -16,23 +23,35 @@ def rreduce(fn, seq, default=None):
     return reduce(fn, seq, default)
 
 
-def layreduce(fn, seq, default=None):
+def reductions(fn, seq, default=nil):
     """LazySeq version of reduce that returns 1 item at a time"""
 
-    def _lzreduce(fn, seq, default=None):
-        accum = default if somep(default) else first(seq)
-        _seq = iter(rest(seq))
-        while True:
-            yield accum
-            fst = first(_seq)
-            if fst is None:
-                raise StopIteration
+    if default is nil:
+        acc = first(seq)
+        x = second(seq)
+        s = rest(rest(seq))
+    else:
+        acc = default
+        x = first(seq)
+        s = rest(seq)
 
-            accum = fn(accum, fst)
-            if not _seq:
-                raise StopIteration
+    yield acc
+    while True:
+        acc = fn(acc, x)
+        x = first(s)
+        s = rest(s)
+        yield acc
+        if len(s) == 0:
+            break
+    yield fn(acc, x)
 
-    return _lzreduce(fn, seq, default)
+
+def cond(x, *forms):
+    if len(forms) % 2 != 0:
+        raise Exception("cond requires an even number of forms!")
+    for t, a in partition(2, forms):
+        if t(x):
+            return a
 
 
 def get_in(d, ks, not_found=None):
@@ -44,7 +63,10 @@ is not present, or the not-found value if supplied.
     :param not_found: what to return if keys not in d
     :return: val
     """
-    return reduce(lambda x, y: x.get(y, {}), ks, d) or not_found
+    if len(ks) == 1:
+        return get(d, first(ks), not_found)
+    else:
+        return get(get(d, first(ks)), rest(ks), not_found)
 
 
 def apply(fn, x):
@@ -52,6 +74,84 @@ def apply(fn, x):
 
     apply(fn, x) --> fn(*x)"""
     return fn(*x)
+
+
+def some(fn, seq=None):
+    """Returns first truthy value or False.  Can accept a predicate as first argument."""
+
+    if not isinstance(fn, (types.FunctionType, types.MethodType)):
+        return some(lambda x: x, seq=fn)
+
+    for e in seq:
+        if fn(e):
+            return e
+    return False
+
+
+def mapv(f, *colls):
+    """Returns a list consisting of the result of applying f to the
+set of first items of each coll, followed by applying f to the set
+of second items in each coll, until any one of the colls is
+exhausted.  Any remaining items in other colls are ignored. Function
+f should accept number-of-colls arguments."""
+    return list(map(f, *colls))
+
+
+def stab(x, *forms):
+    """Equivalent to Clojure's -> macro, requires special syntax (see example)
+
+Threads the expr through the forms. Inserts x as the
+second item in the first form, making a list of it if it is not a
+list already. If there are more forms, inserts the first form as the
+second item in second form, etc.
+
+Example:
+
+>>> stab({}, (assoc, 1, 2), (assoc, 'cat', 'dog'))
+{1: 2, 'cat': 'dog'}
+"""
+    if len(forms) == 0:
+        return x
+    if not isinstance(first(forms), tuple):
+        form = (first(forms),)
+    else:
+        form = first(forms)
+
+    f = first(form)
+    args = (x,) + tuple(rest(form))
+    return stab(f(*args), *rest(forms))
+
+
+# alias in case you don't like the name stab
+threadfirst = threadf = stab
+
+
+def stabb(x, *forms):
+    """Equivalent to Clojure's ->> macro, requires special syntax (see example)
+
+Threads the expr through the forms. Inserts x as the
+last item in the first form, making a list of it if it is not a
+list already. If there are more forms, inserts the first form as the
+last item in second form, etc.
+
+Example:
+>>> stabb(range(10), (filter, lambda x: x % 2 == 0), (map, lambda x: x * 2), list)
+[0, 4, 8, 12, 16]
+"""
+    if len(forms) == 0:
+        return x
+    if not isinstance(first(forms), tuple):
+        form = (first(forms),)
+    else:
+        form = first(forms)
+
+    f = first(form)
+    args = tuple(rest(form)) + (x,)
+    return stabb(f(*args), *rest(forms))
+
+
+# alias in case you don't like the name stabb
+threadlast = threadl = stabb
 
 
 def dec(n):
@@ -66,21 +166,13 @@ def inc(n):
 
 def first(iterable):
     """Returns the first item in the collection. If iterable evaluates to None, returns None."""
-    seq, test = itertools.tee(iterable)
-    try:
-        next(test)
-    except StopIteration:
-        return None
-    return nth(seq, 0)
+    return dispatch(iterable).first(iterable)
 
 
 def nth(seq, idx):
     """Return the nth item of a sequence.  Constant time if list, tuple, or str;
     linear time if a generator or LazySeq"""
-    _, _seq = itertools.tee(seq)
-    if isinstance(seq, seq_types):
-        return seq[idx]
-    return next(itertools.islice(_seq, idx, idx+1))
+    return get(seq, idx)
 
 
 def second(seq):
@@ -136,31 +228,33 @@ def compose(fns, x=None):
     """Takes a set of functions and returns a fn that is the composition
 of those fns.  The returned fn takes a variable number of args,
 applies the rightmost of fns to the args, the next
-fn (right-to-left) to the result, etc.  If no value is supplied, returns a
+fn (left-to-right) to the result, etc.  If no value is supplied, returns a
 stateful transducer"""
     if x is None:
         return partial(compose, fns)
 
-    return rreduce(fn=lambda a, b: b(a),
-                   seq=fns,
-                   default=x)
+    return reduce(lambda a, b: b(a), fns, x)
+
+
+def comp(*fns):
+    """Takes a set of functions and returns a fn that is the composition
+of those fns.  The returned fn takes a variable number of args,
+applies the rightmost of fns to the args, the next
+fn (left-to-right) to the result, etc.  Returns a
+stateful transducer"""
+
+    return partial(reduce, lambda a, b: b(a), reversed(fns))
 
 
 def last(iterable):
     """Return the last item in an iterable, in linear time"""
-    _, seq = itertools.tee(iterable)
-    return next(itertools.islice(seq, -1, None))
+    return dispatch(iterable).last(iterable)
 
 
 def rest(iterable):
     """Returns a LazySeq of the items after the first. Will be an empty list if iterable is empty generator
     or initial item type if empty iterable"""
-    seq, test = itertools.tee(iterable)
-    try:
-        next(test)
-    except StopIteration:
-        return iterable if isinstance(iterable, (seq_types + (dict,))) else []
-    return itertools.islice(iterable, 1, None)
+    return dispatch(iterable).rest(iterable)
 
 
 def iterate(fn, x):
@@ -195,9 +289,23 @@ Returns a stateful transducer when no collection is provided."""
     return itertools.islice(seq, n, None)
 
 
+def gtake(n, seq=None):
+    "A greedy form of take"
+    return list(take(n, seq))
+
+
+def gdrop(n, seq=None):
+    "A greedy form of drop"
+    return list(drop(n, seq))
+
+
 def explode(*ds):
     """Returns a LazySeq of the concatenated (key,value) pairs of the provided dictionaries"""
     return itertools.chain(*map(lambda d: d.items(), ds))
+
+
+def identity(x):
+    return x
 
 
 def merge(*ds):
@@ -212,26 +320,14 @@ def assoc(m, k, v):
   same (hashed/sorted) type, that contains the mapping of key(s) to
   val(s). When applied to a sequence, returns a new sequence of that
   type that contains val v at index k."""
-
-    if isinstance(m, seq_types):
-        return append(m[:k] + [v] + m[k + 1:])
-
-    return merge(m, {k: v})
+    return update(m, k, lambda *_, **__: v)
 
 
 def dissoc(d, *ks):
     """dissoc[iate]. If d is a dict, returns a new map of the same (hashed/sorted) type,
 that does not contain a mapping for key(s).  If d is a str, returns a string without the letters listed as keys.
 For any other sequence type, returns a LazySeq with the listed keys filtered."""
-
-    if isinstance(d, str):
-        filtered = filter(lambda x: x not in ks, d)
-        return ''.join(filtered)
-
-    if isinstance(d, seq_types):
-        return filter(lambda x: x not in ks, d)
-
-    return keyfilter(lambda x: x not in ks, d)
+    return dispatch(d).dissoc(d, *ks)
 
 
 def merge_with(fn, *ds):
@@ -245,10 +341,12 @@ the result by calling fn(val-in-result, val-in-latter)."""
             dict(((k, v) if k not in d else (k, fn(d[k], x[k])) for k, v in x.items())).items())),
         ds)
 
-def merge_with_default(fn, default='null', *dicts):
+
+def merge_with_default(fn, default=nil, *dicts):
     """Like merge_with, except all keys are initialized to default value specified to simplify the collision-fn.
     If no default is specified, will use the initial values of the last dictionary merged."""
-    return merge_with(fn, valmap(lambda v: v if default is 'null' else default, merge(*dicts)), *dicts)
+    return merge_with(fn, valmap(lambda v: v if default is nil else default, merge(*dicts)), *dicts)
+
 
 def assoc_in(d, key_list, val):
     """Associates a value in a nested associative structure, where ks is a
@@ -260,80 +358,273 @@ keys into dictionaries.  I.e.:
 >>> assoc_in(d, [1, 2, 'A'], 'X')
 {1: {2: {3: 4,
         'A': 'X'}}"""
-    d1 = keys2dict(val, *key_list)
-    return recursive_dict_merge(d, d1)
+    if len(key_list) == 1:
+        return assoc(d, first(key_list), val)
+
+    if first(key_list) not in d:
+        return update(d, first(key_list), lambda *_, **__: assoc_in({}, rest(key_list), val))
+
+    return update(d, first(key_list), assoc_in, rest(key_list), val)
+
 
 def terminal_dict(*ds):
     return not (are_dicts(*ds) and all(map(lambda x: are_dicts(*x.values()), ds)))
 
+
 def terminal_dicts(*ds):
     return all(map(terminal_dict, ds))
 
-def update_in(d, key_list, fn):
+
+def get(d, k, not_found=None):
+    return dispatch(d).get(d, k, not_found)
+
+
+def update(d, k, fn, *args, **kwargs):
+    return dispatch(d).update(d, k, fn, *args, **kwargs)
+
+
+def fpartial(f, *args, **kwargs):
+    return lambda x: f(*((x,) + args), **kwargs)
+
+
+class Protocol:
+    @staticmethod
+    def update(d, k, fn, *args, **kwargs):
+        raise NotImplementedError
+
+    @staticmethod
+    def get(d, k, not_found=None):
+        raise NotImplementedError
+
+    @staticmethod
+    def first(d):
+        return next(iter(d))
+
+    @staticmethod
+    def rest(d):
+        raise NotImplementedError
+
+    @staticmethod
+    def last(d):
+        raise NotImplementedError
+
+    @staticmethod
+    def dissoc(d, *ks):
+        raise NotImplementedError
+
+
+class Dict(Protocol):
+    @staticmethod
+    def rest(d):
+        res = iter(d)
+        next(res)
+        return res
+
+    @staticmethod
+    def first(d):
+        return next(iter(k for k in d))
+
+    @staticmethod
+    def update(d, k, fn, *args, **kwargs):
+        return {a: b for a, b in itertools.chain(d.items(), [(k, fn(get(d, k), *args, **kwargs))])}
+
+    @staticmethod
+    def get(d, k, not_found=None):
+        return d.get(k, not_found)
+
+    @staticmethod
+    def last(d):
+        return Dict.first(d)
+
+    @staticmethod
+    def rest(d):
+        return list(d)[1:] or {}
+
+    @staticmethod
+    def dissoc(d, *ks):
+        ks = set(ks)
+        return keyfilter(lambda x: x not in ks, d)
+
+
+class List(Protocol):
+    @staticmethod
+    def get(d, k, not_found=None):
+        return d[k]
+
+    @staticmethod
+    def update(d, k, fn, *args, **kwargs):
+        return d[:k] + [fn(get(d, k), *args, **kwargs)] + d[k + 1:]
+
+    @staticmethod
+    def last(d):
+        return d[-1]
+
+    @staticmethod
+    def rest(d):
+        return d[1:]
+
+    @staticmethod
+    def dissoc(d, *ks):
+        ks = set(ks)
+        return filterv(lambda x: x not in ks, d)
+
+
+class Tuple(Protocol):
+    @staticmethod
+    def dissoc(d, *ks):
+        ks = set(ks)
+        return tuple(filter(lambda x: x not in ks, d))
+
+    @staticmethod
+    def get(d, k, not_found=None):
+        return d[k]
+
+    @staticmethod
+    def update(d, k, fn, *args, **kwargs):
+        return d[:k] + (fn(get(d, k), *args, **kwargs),) + d[k + 1:]
+
+    @staticmethod
+    def last(d):
+        return d[-1]
+
+    @staticmethod
+    def rest(d):
+        return d[1:]
+
+
+class String(Protocol):
+    @staticmethod
+    def last(d):
+        return d[-1]
+
+    @staticmethod
+    def get(d, k, not_found=None):
+        return d[k]
+
+    @staticmethod
+    def update(d, k, fn, *args, **kwargs):
+        return d[:k] + fn(get(d, k), *args, **kwargs) + d[k + 1]
+
+    @staticmethod
+    def rest(d):
+        return d[1:]
+
+    @staticmethod
+    def dissoc(d, *ks):
+        return ''.join(filter(lambda x: x not in ks, d))
+
+
+class Iterable(Protocol):
+    @staticmethod
+    def last(d):
+        try:
+            for x in d:
+                pass
+            return x
+        except NameError:
+            return None
+
+    @staticmethod
+    def first(d):
+        return next(iter(d))
+
+    @staticmethod
+    def rest(d):
+        try:
+            next(d)
+            return d
+        except StopIteration:
+            return None
+
+
+class Cons(Protocol):
+    @staticmethod
+    def first(d):
+        return d(0)
+
+
+def update_in(d, key_list, fn, *args, **kwargs):
     """'Updates' a value in a nested associative structure, where ks is a
 sequence of keys and f is a function that will take the old value
 and any supplied args and return the new value, and returns a new
 nested structure.  If any levels do not exist, hash-maps will be
 created."""
+    if len(key_list) == 1:
+        return update(d, first(key_list), fn, *args, **kwargs)
 
-    v = fn(get_in(d, key_list))
-    d1 = keys2dict(v, *key_list)
-    return merge_with(lambda a, b: recursive_dict_merge(a, b) if not are_dicts(a, b) else b, d, d1)
+    return update(d, first(key_list), update_in, rest(key_list), fn, *args, **kwargs)
+
+
+def dispatch(x, dispatch_table=None):
+    Table = dispatch_table or (fpartial(isinstance, collections.MutableMapping), Dict,
+                               fpartial(isinstance, str), String,
+                               fpartial(isinstance, list), List,
+                               fpartial(isinstance, tuple), Tuple,
+                               lambda _: True, type(x))
+
+    return cond(x, *Table)
+
 
 def recursive_dict_merge(*ds):
     """Recursively merge dictionaries"""
     return merge_with(lambda a, b: recursive_dict_merge(a, b) if not terminal_dicts(a, b) else merge(a, b), *ds)
+
 
 def keys2dict(val, *ks):
     """Convert a value and a list of keys to a nested dictionary with the value at the leaf"""
     v_in = reduce(lambda x, y: {y: x}, (list(ks) + [val])[::-1])
     return v_in
 
+
 def are_instances(items, types):
     """Plural of isinstance"""
     return all(map(lambda x: isinstance(x, types), items))
 
+
 def are_dicts(*ds):
     return are_instances(ds, dict)
+
 
 def supassoc_in(d, val, k, *ks):
     """Like assoc_in, except collisions are handled by grouping into a list rather than merging"""
     ds = d, keys2dict(val, *itertools.chain([k], ks))
     return recursive_group_dicts(*ds)
 
+
 def recursive_group_dicts(*ds):
     """Like recursive_dict_merge, except handles recursive collisions by grouping into a list instead of merging"""
     return merge_with(lambda x, y: recursive_group_dicts(x, y) if not terminal_dicts(x, y) else {
         first(x.keys()): x.values() + y.values()}, *ds)
 
+
 def keyfilter(fn, d):
-    # TODO: make purely functional when persistent vectors are finished
     return {k: v for k, v in d.items() if fn(k)}
+
 
 def valfilter(fn, d):
     """returns {k: v for k, v in d.items() if fn(v)}"""
-    # TODO: make purely functional when persistent vectors are finished
     return {k: v for k, v in d.items() if fn(v)}
+
 
 def itemfilter(fn, d):
     """returns {k: v for k, v in d.items() if fn(k, v)}"""
-    # TODO: make purely functional when persistent vectors are finished
     return {k: v for k, v in d.items() if fn(k, v)}
+
 
 def valmap(fn, d):
     """{k: fn(v) for k, v in d.items()}"""
-    # TODO: make purely functional when persistent vectors are finished
     return {k: fn(v) for k, v in d.items()}
+
 
 def keymap(fn, d):
     """returns {fn(k): v for k, v in d.items()}"""
-    # TODO: make purely functional when persistent vectors are finished
     return {fn(k): v for k, v in d.items()}
+
 
 def itemmap(fn, d):
     """returns dict(fn(k, v) for k, v in d.items())"""
-    # TODO: make purely functional when persistent vectors are finished
     return dict(fn(k, v) for k, v in d.items())
+
 
 def nary(fn):
     """fn(a, b) --> fn(*x).  Only works if the output type is the same as the
@@ -349,36 +640,53 @@ def nary(fn):
 
     return _nary
 
+
 def append(*seqs):
-    """Returns a LazySeq concatenation of iterables (works in constant time)."""
-    return itertools.chain(*seqs)
+    """Join sequences"""
+    return list(itertools.chain(*seqs))
+
 
 def pop(iterable):
     _, seq = itertools.tee(iterable)
     return itertools.islice(0, len(seq) - 1)
+
 
 def windows(n, seq):
     """Returns a lazy sequence of lists of n items each"""
     if 'zip_longest' in dir(itertools):
         return itertools.zip_longest(*(seq[i::n] for i in range(n)))
     else:
-        return itertools.izip_longest(*(set[i::n] for i in range(n)))
+        return itertools.izip_longest(*(seq[i::n] for i in range(n)))
+
 
 def partition(n, seq):
     """Returns a lazy sequence of lists of n items each"""
     return windows(n, seq)
 
+
 def conj(seq, *items):
-    return itertools.chain(seq, items)
+    return list(itertools.chain(seq, items))
+
 
 def nonep(x):
     return x is None
 
+
 def complement(x):
     return not x
+
 
 def somep(x):
     return complement(nonep(x))
 
-if __name__ == '__main__':
-    print('test')
+
+def filterv(fn, *colls):
+    "A greedy version of filter"
+    return list(filter(fn, *colls))
+
+
+Result = collections.namedtuple('Result', field_names=['args', 'kwargs'])
+
+
+def ffirst(x):
+    return first(first(x))
--- python_code/functional_tools/tools_test.py
+++ python_code/functional_tools/tools_test.py
@@ -1,10 +1,11 @@
+import operator
 import unittest
 
-from python_code.functional_tools.tools import apply, merge, assoc, dissoc, merge_with, merge_with_default, assoc_in, \
-    update_in, terminal_dict
+from python_code.functional_tools.tools import *
 
 
-class FuncyToolsTest(unittest.TestCase):
+class FunkyToolsTest(unittest.TestCase):
+
     def setUp(self):
         self.ds = a, b, c = [
             {1: 2,
@@ -19,7 +20,222 @@ class FuncyToolsTest(unittest.TestCase):
     def test_explode_convs_list_dicts_to_list_tuples(self):
         ds = self.ds
         expected_res = {(1, 2), (3, 4), (4, 5), ('a', 'b'), ('c', 'd'), ('e', 'f'), ('name', 'taco')}
-        res = set(apply(*ds))
+        res = set(apply(explode, ds))
+        self.assertEqual(expected_res, res)
+
+    def test_merge_merges_dicts(self):
+        ds = self.ds
+        expected_res = {
+            1: 2,
+            3: 4,
+            4: 5,
+            'a': 'b',
+            'c': 'd',
+            'e': 'f',
+            'name': 'taco'
+        }
+
+        res = merge(*ds)
+
+        self.assertEqual(expected_res, res)
+
+    def test_assoc_assocs_kvs_into_dict(self):
+        _, _, c = self.ds
+
+        expected_res = {'name': 'taco', 'num': 42}
+        res = assoc(c, 'num', 42)
+        self.assertEqual(expected_res, res)
+
+    def test_dissoc_removes_kvs_from_dict(self):
+        a, _, _ = self.ds
+        expected_res = {3: 4, 4: 5}
+        res = dissoc(a, 1)
+        self.assertEqual(expected_res, res)
+
+    def test_merge_with_merges_only_ints(self):
+        a, _, c = self.ds
+        expected_res = {1: 4, 3: 8, 4: 10, 'name': 'taco'}
+        res = merge_with(lambda x, y: x + y if (isinstance(x, int) and isinstance(y, int)) else y, a, a, c, c)
+        self.assertEqual(expected_res, res)
+
+    def test_merge_with_doubles_strings_only(self):
+        a, _, c = self.ds
+        expected_res = {1: 2, 3: 4, 4: 5, 'name': 'tacotaco'}
+        res = merge_with(lambda x, y: x + y if isinstance(x, str) else y,
+                         a, a, c, c)
+        self.assertEqual(expected_res, res)
+
+    def test_merge_with_default_cats_to_list(self):
+        ds = self.ds
+        expected_res = {1: [2, 2], 3: [4, 4], 4: [5, 5], 'a': ['b', 'b'], 'c': ['d', 'd'], 'e': ['f', 'f'],
+                        'name': ['taco', 'taco']}
+        res = merge_with_default(lambda x, y: x + [y], [], *(ds + ds))
+        self.assertEqual(expected_res, res)
+
+    def test_merge_with_default_sums_or_adds(self):
+        a = first(self.ds)
+        expected_res = {1: 4, 3: 8, 4: 10}
+        res = merge_with_default(lambda x, y: x + y, 0, *[a, a])
+        self.assertEqual(expected_res, res)
+
+    def test_assoc_in_non_destructively_adds_key_to_path(self):
+        _, _, c = self.ds
+        expected_res = {'name': 'taco',
+                        'feelings': {'they': {'are': 'mutual'}}}
+
+        res = assoc_in(c, ['feelings', 'they', 'are'], 'mutual')
+        self.assertEqual(expected_res, res)
+
+    def test_assoc_in_merges_when_same_level_as_nonterminal_dict(self):
+        _, _, c = self.ds
+        initial_d = assoc_in(c, ['thisis'], {'a': dict})
+        expected_res = {'name': 'taco',
+                        'thisis': {'a': dict,
+                                   'b': dict}
+                        }
+        res = assoc_in(initial_d, ['thisis', 'b'], dict)
+        self.assertEqual(expected_res, res)
+
+    def test_assoc_in_works_with_mixed_datatypes(self):
+        d0 = {'a': {'c': [1, 2, 3],
+                    'd': {'e': [4, 5, 6]}}}
+
+        expected_res = {'a': {'c': [1, 2, 3],
+                              'd': {'e': [4, 5, 6],
+                                    'f': {'g': [9, 10, 11]}}}}
+
+        res = assoc_in(d0, 'adfg', [9, 10, 11])
+        self.assertEqual(expected_res, res)
+
+    def test_assoc_in_adds_dict_okay(self):
+        d0 = {'a': {'c': [1, 2, 3],
+                    'd': {'e': [4, 5, 6]}}}
+        expected_res = {'a': {'c': [1, 2, 3],
+                              'd': {'e': [4, 5, 6],
+                                    'f': {'1': '2', '3': 'hello'}}}}
+
+        res = assoc_in(d0, ['a', 'd', 'f'], {'1': '2', '3': 'hello'})
+        self.assertEqual(expected_res, res)
+
+    def test_update_in_destructively_adds_key_to_path(self):
+        _, _, c = self.ds
+        initial_d = assoc_in(c, ['thisis'], {'a': dict})
+        expected_res = {'name': 'taco',
+                        'thisis': {'b': dict}}
+        res = update_in(initial_d, ['thisis'], lambda x: {'b': dict})
+        self.assertEqual(expected_res, res)
+
+    def test_terminal_dicts_id_terminal_dicts_correctly(self):
+        a = {'a': ['this', 'is', 'terminal']}
+        b = {'b': {'this': 'ain\'t'}}
+        self.assertTrue(terminal_dict(a))
+        self.assertFalse(terminal_dict(b))
+
+    def test_windows_creates_groups_of_3s(self):
+
+        lst = range(1, 13)
+        expected_res = [(1, 2, 3),
+                        (4, 5, 6),
+                        (7, 8, 9),
+                        (10, 11, 12)]
+        res = list(windows(3, lst))
+        self.assertEqual(expected_res, res)
+
+    def test_windows_creates_groups_of_1s(self):
+        lst = range(5)
+        expected_res = [(x,) for x in lst]
+        res = list(windows(1, lst))
+        self.assertEqual(expected_res, res)
+
+    def test_append_concatenates_lists(self):
+        lsts = range(10), range(10), range(10)
+        expected_res = reduce(lambda x, y: list(x) + list(y), lsts)
+        res = list(append(*map(list, lsts)))
+        self.assertEqual(expected_res, res)
+
+    def test_conjugate_conjugates_into_lst(self):
+
+        lst = list('abc')
+        appendos = 'def'
+        expected_res = list('abcdef')
+        res = list(conj(lst, *appendos))
+        self.assertEqual(expected_res, res)
+
+    def test_first_gets_first_item_of_list(self):
+        a = [1]
+        expected_res = 1
+        res = first(a)
+        self.assertEqual(expected_res, res)
+
+    def test_first_gets_first_item_of_tuple(self):
+        a = 1,
+        expected_res = 1
+        res = first(a)
+        self.assertEqual(expected_res, res)
+
+    def test_nth_returns_nth_item(self):
+
+        a = [1, 2, 3]
+        expected_res = 3
+        res = nth(a, 2)
+        self.assertEqual(expected_res, res)
+
+    def test_rest_returns_none_on_empty_seq(self):
+
+        seqs = [], tuple(), {}, ''
+        expected_res = list(seqs)
+        res = [rest(x) for x in seqs]
+        self.assertEqual(expected_res, res)
+
+    def test_layreduce_sums_numbers(self):
+
+        nums = list(range(5))
+        expected_res = [0, 1, 3, 6, 10]
+        res = list(reductions(fn=operator.add,
+                              seq=nums))
+        self.assertEqual(expected_res, res)
+
+    def test_first_returns_first_on_str(self):
+        a = 'hey'
+        expected_res = 'h'
+        res = first(a)
+        self.assertEqual(expected_res, res)
+
+    def test_iterate_iterates_over_fn(self):
+
+        double = lambda x: x * 2
+        expected_res = [2, 4, 8, 16, 32]
+        res = list(take(5, iterate(double, 2)))
+        self.assertEqual(expected_res, res)
+
+    def test_take_takes_first_10_of_inf_seq(self):
+
+        expected_res = list(range(10))
+        res = list(take(10, itertools.count()))
+        self.assertEqual(expected_res, res)
+
+    def test_drop_drops_first_5_of_inf_seq(self):
+        expected_res = list(range(5, 10))
+        res = list(take(5, drop(5, itertools.count())))
+        self.assertEqual(expected_res, res)
+
+
+class FuncyToolsTest2(unittest.TestCase):
+    def setUp(self):
+        self.ds = a, b, c = [
+            {1: 2,
+             3: 4,
+             4: 5},
+            {'a': 'b',
+             'c': 'd',
+             'e': 'f'},
+            {'name': 'taco'}
+        ]
+
+    def test_explode_convs_list_dicts_to_list_tuples(self):
+        ds = self.ds
+        expected_res = {(1, 2), (3, 4), (4, 5), ('a', 'b'), ('c', 'd'), ('e', 'f'), ('name', 'taco')}
+        res = set(explode(*ds))
         self.assertEqual(expected_res, res)
 
     def test_merge_merges_dicts(self):
@@ -114,7 +330,7 @@ class FuncyToolsTest(unittest.TestCase):
                               'd': {'e': [4, 5, 6],
                                     'f': {'1': 2, '3': 'hello'}}}}
 
-        res = assoc_in(d0, ['a', 'd', 'f'], {'1': '2', '3': 'hello'})
+        res = assoc_in(d0, ['a', 'd', 'f'], {'1': 2, '3': 'hello'})
         self.assertEqual(expected_res, res)
 
     def test_update_in_destructively_adds_key_to_path(self):
@@ -122,7 +338,7 @@ class FuncyToolsTest(unittest.TestCase):
         initial_d = assoc_in(c, ['thisis'], {'a': dict})
         expected_res = {'name': 'taco',
                         'thisis': {'b': dict}}
-        res = update_in(initial_d, ['thisis', 'b'], dict)
+        res = update_in(initial_d, ['thisis'], lambda x: {'b': dict})
         self.assertEqual(expected_res, res)
 
     def test_terminal_dicts_id_terminal_dicts_correctly(self):
@@ -130,3 +346,6 @@ class FuncyToolsTest(unittest.TestCase):
         b = {'b': {'this': 'ain\'t'}}
         self.assertTrue(terminal_dict(a))
         self.assertFalse(terminal_dict(b))
+
+if __name__ == '__main__':
+    unittest.main()
